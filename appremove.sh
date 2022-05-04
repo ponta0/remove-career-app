392f11040d57bf2a73ef5fb9f23b3bd0bca99b80
まぐにあ <dev@mail.mag-neko.net>
2022-05-04 09:11:47 +0900

    完全に書き直した

--- appremove.sh
+++ appremove.sh
@@ -1,66 +1,121 @@
-#!/bin/bash
+#! /bin/bash
+declare -a carrier_app_name=("docomo" "ntt" "auone" "rakuten" "kddi" "softbank" "taginfo") 
 
-if type "adb" > /dev/null 2>&1; then
-  adb shell exit > /dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    adb shell pm list package >> pkg.txt
-    cat pkg.txt | sed -e "s/package://g" | grep 'docomo' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat pkg.txt | sed -e "s/package://g" | grep 'ntt' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat pkg.txt | sed -e "s/package://g" | grep 'auone' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat pkg.txt | sed -e "s/package://g" | grep 'rakuten' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat pkg.txt | sed -e "s/package://g" | grep 'kddi' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat pkg.txt | sed -e "s/package://g" | grep 'softbank' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-    cat rm.sh | sed -e "s/adb shell pm uninstall --user 0//g"
-    echo -n "以上のアプリが消去されます [Y/n]: "
-    read ANS
-    case $ANS in
-      "" | [Yy]* )
-        sh rm.sh
-        rm rm.sh pkg.txt
-        echo "アプリの消去を実行しました"
-        ;;
-      * )
-        rm rm.sh pkg.txt
-        echo "処理を中止しました。"
-        ;;
-    esac
-  elif [ $? -eq 1 ]; then
-    echo "USBデバッグが有効なデバイスが見つかりません。"
-    echo "Android端末が正しく接続されているか確認してください"
-  fi
-else
-  echo "adbコマンドが存在しません。"
-  echo "コマンドを自動でインストールします"
-  sudo apt install adb -y
-  if [ $? -eq 0 ]; then
-    adb shell exit > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
-      adb shell pm list package >> pkg.txt
-      cat pkg.txt | sed -e "s/package://g" | grep 'docomo' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat pkg.txt | sed -e "s/package://g" | grep 'ntt' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat pkg.txt | sed -e "s/package://g" | grep 'auone' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat pkg.txt | sed -e "s/package://g" | grep 'rakuten' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat pkg.txt | sed -e "s/package://g" | grep 'kddi' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat pkg.txt | sed -e "s/package://g" | grep 'softbank' | sed "s/^/adb shell pm uninstall --user 0  /g" >> rm.sh
-      cat rm.sh | sed -e "s/adb shell pm uninstall --user 0//g"
-      echo -n "以上のアプリが消去されます [Y/n]: "
-      read ANS
-      case $ANS in
-        "" | [Yy]* )
-          sh rm.sh
-          rm rm.sh pkg.txt
-          echo "アプリの消去を実行しました"
-          ;;
-        * )
-          rm rm.sh pkg.txt
-          echo "処理を中止しました。"
-          ;;
-      esac
-    elif [ $? -eq 1 ]; then
-      echo "USBデバッグが有効なデバイスが見つかりません。"
-      echo "Android端末が正しく接続されているか確認してください"
+# adbが存在するか確認
+which adb > /dev/null 2>&1
+if [ $? -eq 1 ]
+then
+    echo "adbコマンドが存在しません"
+    echo -n "adbコマンドを自動でインストールしますか? [y/n] : "
+    read answer
+    if [ $answer = "y" ] || [ $answer = "Y" ]
+    then
+        echo "adbコマンドをインストールします"
+        case `uname -s` in
+            Darwin)
+                brew install android-platform-tools
+                ;;
+            Linux)
+                # redhatかarchかubuntuか
+                if [ -e /etc/redhat-release ]
+                then
+                    sudo yum install android-tools
+                elif [ -e /etc/arch-release ]
+                then
+                    sudo pacman -S android-tools
+                elif [ -e /etc/lsb-release ]
+                then
+                    sudo apt-get install android-tools-adb
+                else
+                    echo "このOSは対応していません"
+                    exit 1
+                fi
+                ;;
+            *)
+                echo "このOSは対応していません"
+                exit 1
+                ;;
+        esac
+    elif [ $answer = "n" ] || [ $answer = "N" ]
+    then
+        echo "adbコマンドをインストールを中止しました"
+        exit 0
+    else
+        echo "不正な入力です"
+        echo "処理を中止しました"
+        exit 1
     fi
-  elif [ $? -eq 1 ]; then
-    echo "adbのインストールに失敗しました。"
-  fi
+fi
+
+# デバイスが接続されているか確認
+adb shell exit > /dev/null 2>&1
+if [ $? -eq 1 ]
+then
+    echo "デバイスが接続されていません"
+    exit 1
+fi
+
+adb shell pm list package >> pkg.txt
+adb shell pm list package -f >> backup.txt
+
+# パッケージ名の一覧を取得
+for i in "${carrier_app_name[@]}"
+do
+    cat pkg.txt | sed "s/package://g" | grep $i >> name.txt
+done
+rm pkg.txt
+
+# パッケージのパスを取得
+for i in "${carrier_app_name[@]}"
+do
+    cat backup.txt | sed "s/package://g" | sed "s/apk=.*$//g" | sed "s/$/apk/g" | grep $i >> path.txt
+done
+rm backup.txt
+
+# バックアップスクリプトを生成
+paste path.txt name.txt | sed "s/^/adb pull /g" | sed "s/$/.apk/g" > backup.sh
+echo "mkdir backup" >> backup.sh
+echo "mv *.apk backup" >> backup.sh
+rm path.txt
+
+# 削除スクリプトを生成
+cat name.txt | sed "s/^/adb shell pm uninstall --user 0 /g" > remove.sh
+
+# バックアップスクリプトを実行
+cat name.txt
+echo -n "アプリのバックアップを取得しますか? [Y/n] : "
+read answer
+if [ $answer = "Y" ] || [ $answer = "y" ]
+then
+    echo "スマホでバックアップを認可してください"
+    sh backup.sh
+    rm backup.sh
+    echo "アプリのバックアップを実行しました"
+elif [ $answer = "N" ] || [ $answer = "n" ]
+then
+    rm backup.sh 
+else
+    rm backup.sh name.txt remove.sh
+    echo "不正な入力です"
+    echo "処理を中止しました"
+    exit 1
+fi
+
+# 削除スクリプトを実行
+cat name.txt
+echo -n "アプリを削除しますか? [Y/n] : "
+read answer
+if [ $answer = "Y" ] || [ $answer = "y" ]
+then
+    sh remove.sh
+    rm remove.sh name.txt
+    echo "アプリを削除しました"
+elif [ $answer = "N" ] || [ $answer = "n" ]
+then
+    rm remove.sh name.txt
+else
+    rm remove.sh name.txt
+    echo "不正な入力です"
+    echo "処理を中止しました"
+    exit 1
 fi
